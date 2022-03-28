e3264388e9440feeb57967be67666a06079884d8
Colin Bester <bester.colin@gmail.com>
2022-03-28 05:57:51 -0500

    dropdown test

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
+import React from 'react';
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Modal, Button, SafeAreaView } from 'react-native';
+import CategoryFiltersTest from './components/CategoryFiltersTest';
 
 export default function App() {
+  const [showMT, setShowMT] = useState(false)
+
+  const data = [
+    { id: "1", label: "item1" },
+    { id: "2", label: "item2" },
+    { id: "3", label: "item3" },
+  ]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="show" onPress={() => setShowMT(true)} />
+      <Modal transparent={false} visible={showMT} supportedOrientations={['portrait', 'landscape']}>
+        <SafeAreaView>
+          <CategoryFiltersTest
+            data={data}
+          />
+          <Button title="close" onPress={() => setShowMT(false)} />
+        </SafeAreaView>
+      </Modal>
       <StatusBar style="auto" />
     </View>
   );
